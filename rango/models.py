e559667650774056ae44e0b3252b8febca3a05b2
Weixuan Li <2512192l@student.gla.ac.uk>
2021-07-29 02:29:33 +0800

    Chapter 6

--- rango/models.py
+++ rango/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.template.defaultfilters import slugify
 # Create your models here.
 class Category(models.Model):
-    name=models.CharField(max_length=128,unique=True)
-    views=models.IntegerField(default=0)
-    likes=models.IntegerField(default=0)
-    slug=models.SlugField(unique=True)
+    name = models.CharField(max_length=128,unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
     def save(self,*args,**kwares):
         self.slug=slugify(self.name)
         super(Category,self).save(*args,**kwares)
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
-from rango.models import Category
+from rango.models import Category,Page
 def index(request):
 
     category_list=Category.objects.order_by('-likes')[:5]
@@ -8,5 +8,18 @@ def index(request):
     context_dict['boldmessage']= 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories']=category_list
     return render(request, 'rango/index.html', context=context_dict)
+
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request,category_name_slug):
+    context_dic={}
+    try:
+        category=Category.objects.get(slug=category_name_slug)
+        pages=Page.objects.filter(category=category)
+        context_dic['pages']=pages
+        context_dic['category']=category
+    except Category.DoesNotExist:
+        context_dic['category']=None
+        context_dic['pages']=None
+    return render(request, 'rango/category.html', context=context_dic)
