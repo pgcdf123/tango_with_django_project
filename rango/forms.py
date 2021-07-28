e20a2eb27e9fcc291e8d8426f401c4d26913df24
Weixuan Li <2512192l@student.gla.ac.uk>
2021-07-29 05:06:50 +0800

    Chapter 7

--- rango/forms.py
+++ rango/forms.py
@@ -1,24 +1,24 @@
 from django import forms
 from rango.models import Page, Category
 class CategroyForm(forms.ModelForm):
-    name=forms.CharField(max_length=128,help_text="please enter the category name.")
-    views=forms.IntegerField(widget=forms.HiddenInput(),initial=0)
-    likes=forms.IntegerField(widget=forms.HiddenInput(),initial=0)
-    slug=forms.CharField(widget=forms.HiddenInput(),required=False)
+    name = forms.CharField(max_length=128, help_text="please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
     class Mete:
-        model=Category
-        fields=('name',)
+        model = Category
+        fields = ('name',)
 class PageForm(forms.ModelForm):
-    title=forms.CharField(max_length=128,help_text="please enter the title of the page")
-    url=forms.URLField(max_length=200,help_text="please enter the URL of the page")
-    views=forms.IntegerField(widget=forms.HiddenInput(),initial=0)
+    title = forms.CharField(max_length=128, help_text="please enter the title of the page")
+    url = forms.URLField(max_length=200, help_text="please enter the URL of the page")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     class Meta:
-        model=Page
-        exclude=('category',)
+        model = Page
+        exclude = ('category',)
     def clean(self):
-        cleaned_data=self.cleaned_data
-        url=cleaned_data.get('url')
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
         if url and not url.startwith('http://'):
-            url=f'http://{url}'
-            cleaned_data['url']=url
+            url = f'http://{url}'
+            cleaned_data['url'] = url
         return cleaned_data
