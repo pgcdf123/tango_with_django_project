b724eb031fc078cd67dfb071a2da39537d1b97b2
Weixuan Li <2512192l@student.gla.ac.uk>
2021-07-30 05:40:45 +0800

    Chapter 10 and amend something about chapter 3-9

--- rango/views.py
+++ rango/views.py
@@ -19,9 +19,9 @@ def index(request):
     return response
 
 def about(request):
-    visit_cookie={}
+    visit_cookie = {}
     visitor_cookie_handler(request)
-    visit_cookie['visit']=request.session['visit']
+    visit_cookie['visits'] = request.session['visits']
     return render(request, 'rango/about.html', context=visit_cookie)
 
 def show_category(request,category_name_slug):
@@ -125,18 +125,19 @@ def restricted(requset):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
-def visitor_cookie_handler(request,response):
-    visits=int(request.COOKIES.get('visit','1'))
-    last_visit_cookie=get_server_side_cookie(request,'last_visit',str(datetime.now()))
-    last_visit_time=datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
-    if(datetime.now()-last_visit_time).days>0:
-        visits=visits+1
-        request.session['last_visit']=str(datetime.now())
-    else:
-        request.session['last_visit']=last_visit_cookie
-    request.session['visit']=visits
 def get_server_side_cookie(request,cookie,default_val=None):
-    val=request.session.get(cookie)
+    val = request.session.get(cookie)
     if not val:
-        val=default_val
+        val = default_val
     return val
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request,'visits','1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    if(datetime.now()-last_visit_time).days > 0:
+        visits=visits+1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
