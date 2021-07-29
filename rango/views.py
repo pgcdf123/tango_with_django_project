e9cd1a04b305ba5ce2b03ad5f1b5a2c0d5befd46
Weixuan Li <2512192l@student.gla.ac.uk>
2021-07-30 05:19:05 +0800

    Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from rango.forms import CategroyForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
     category_list=Category.objects.order_by('-likes')[:5]
@@ -13,10 +14,15 @@ def index(request):
     context_dict['boldmessage']= 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories']=category_list
     context_dict['pages']=page_list
-    return render(request, 'rango/index.html', context=context_dict)
+
+    response=render(request,'rango/index.html',context=context_dict)
+    return response
 
 def about(request):
-    return render(request, 'rango/about.html')
+    visit_cookie={}
+    visitor_cookie_handler(request)
+    visit_cookie['visit']=request.session['visit']
+    return render(request, 'rango/about.html', context=visit_cookie)
 
 def show_category(request,category_name_slug):
     context_dic = {}
@@ -119,3 +125,18 @@ def restricted(requset):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+def visitor_cookie_handler(request,response):
+    visits=int(request.COOKIES.get('visit','1'))
+    last_visit_cookie=get_server_side_cookie(request,'last_visit',str(datetime.now()))
+    last_visit_time=datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    if(datetime.now()-last_visit_time).days>0:
+        visits=visits+1
+        request.session['last_visit']=str(datetime.now())
+    else:
+        request.session['last_visit']=last_visit_cookie
+    request.session['visit']=visits
+def get_server_side_cookie(request,cookie,default_val=None):
+    val=request.session.get(cookie)
+    if not val:
+        val=default_val
+    return val
